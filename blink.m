34cf149646e84947c4270b9ab37ddbc6557809c0
Leo Pauly <meetleopauly@yahoo.com>
2016-03-26 15:38:34 +0530

    Update blink.m

--- blink.m
+++ blink.m
@@ -10,12 +10,24 @@ reright=predict(cl,teright);
 
 f=reright && releft;
 if(f==1)
-    word='active';
+    word='eyes are active';
 else
-    word='dozing';
+    word='eyes closing/blinking';
 end
 
-l=int2str(releft);
-r=int2str(reright);
+if(releft==1)
+    l='this eye is open';
+else
+    l='this eye is closed';
+end
+
+if(reright==1)
+    r='this eye is open';
+else
+    r='this eye is closed';
+end
+
+%l=int2str(releft);
+%r=int2str(reright);
 
 end
